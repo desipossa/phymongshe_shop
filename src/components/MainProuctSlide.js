ecb81bb455ac6aee274905af2203671d6a0b7e04
lee <sipossa@naver.com>
2022-09-30 09:51:02 +0900

    20220930

--- src/components/MainProuctSlide.js
+++ src/components/MainProuctSlide.js
@@ -4,9 +4,13 @@ import MainCategory from './MainCategory'
 
 const MainProuctSlide = ({ shopList, category }) => {
     return (
-        <div>
-            <CategorySlider shopList={shopList} category={category} />
-        </div>
+        <section className="sce slide_">
+            <h2>BEST PRODUCT</h2>
+            <p>피몽쉐 마인드풀 라운지 BEST PRODUCT</p>
+            <div className="inner">
+                <CategorySlider shopList={shopList} category={category} arrow={true} dots={true} />
+            </div>
+        </section>
     )
 }
 
