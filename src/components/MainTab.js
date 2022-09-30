643a221ea33bfd4b19d1b83269cd3dc01484ad05
lee <sipossa@naver.com>
2022-09-30 17:46:17 +0900

    20220930

--- src/components/MainTab.js
+++ src/components/MainTab.js
@@ -3,22 +3,25 @@ import { Link } from 'react-router-dom'
 import MainCategory from './MainCategory'
 import CategorySlider from './CategorySlider'
 
-const MainTab = ({ shopList }) => {
+const MainTab = ({ shopList, arrow, dots }) => {
     const link = [
         'pencil', 'liquid', 'powder'
     ];
     const [alink, setAlink] = useState(0);
     return (
-        <section className='sc'>
+        <section className='sce slide_'>
             <h2>Best Product</h2>
             <ul className='bp_tab'>
                 {
                     link.map((it, idx) => {
-                        return <li onClick={() => setAlink(idx)}>{it}</li>
+                        return <li onClick={() => setAlink(idx)} key={idx}>{it}</li>
                     })
                 }
             </ul>
-            <CategorySlider shopList={shopList} category={link[alink]} />
+            <div className="inner">
+                <CategorySlider shopList={shopList} category={link[alink]} arrow={arrow} dots={dots} />
+            </div>
+
 
         </section>
     )
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,10 +12,9 @@ const Main = ({ shopList }) => {
             <MainSlide />
             <MainProuctSlide shopList={shopList} category={'pencil'} tit={'BEST PRODUCT'} des={'피몽쉐 마인드풀 라운지 BEST PRODUCT'} arrow={true} dots={true} />
             <MainProuctSlide shopList={shopList} category={'liquid'} tit={'NEW PRODUCT'} des={'피몽쉐 마인드풀 라운지 new PRODUCT'} arrow={true} dots={false} bg />
-            <MainTab shopList={shopList} />
-            <MainCategory shopList={shopList} category={'pencil'} />
-            <MainCategory shopList={shopList} category={'liquid'} />
             <MainCategory shopList={shopList} category={'powder'} />
+            <MainCategory shopList={shopList} category={'liquid'} bg />
+            <MainTab shopList={shopList} arrow={true} dots={false} />
         </>
     )
 }
